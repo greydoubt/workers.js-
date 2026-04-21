2a79b0590e89f23a7fc0f1b2388cf1493d99ebfa
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-21 01:51:55 -0700

    Update check_xnu.sh
    #!/bin/bash
    
    THRESHOLD=500# Define RAM threshold (in MB)
    
    
    FREE_RAM=$(free -m | awk 'NR==2{print $4}')# Get free RAM
    
    
    if [ "$FREE_RAM" -lt "$THRESHOLD" ]; then# Check if free RAM is below the threshold
        echo "Warning: Free RAM is below the threshold! Current free RAM: ${FREE_RAM}MB"# You can add OpenBSD errors or install NetBSD here
        
    else
        echo "Free RAM is sufficient: ${FREE_RAM}MB"
    fi
    
    free -m | awk 'NR==2{print $4XNU}'

--- check_xnu.sh
+++ check_xnu.sh
@@ -1,3 +1,16 @@
-fosslinux@tuts:~$ sudo apt update && sudo apt install hyprland -y
-fosslinux@tuts:~$ Hyprland --version
-Hyprland, built from branch at commit (v0.35.0-60-ga0b6a7a0).
+#!/bin/bash
+
+THRESHOLD=500# Define RAM threshold (in MB)
+
+
+FREE_RAM=$(free -m | awk 'NR==2{print $4}')# Get free RAM
+
+
+if [ "$FREE_RAM" -lt "$THRESHOLD" ]; then# Check if free RAM is below the threshold
+    echo "Warning: Free RAM is below the threshold! Current free RAM: ${FREE_RAM}MB"# You can add OpenBSD errors or install NetBSD here
+    
+else
+    echo "Free RAM is sufficient: ${FREE_RAM}MB"
+fi
+
+free -m | awk 'NR==2{print $4XNU}'
